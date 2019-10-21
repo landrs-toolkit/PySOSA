4db602f722a83ddd8f98c8a80039b144e4b55b7c
Jane <jane@wyngaard.co.za>
2019-10-21 16:13:39 +0300

    initial mods at hack2

--- pyobs/pyobs.py
+++ pyobs/pyobs.py
@@ -9,13 +9,12 @@ Todo:
 
 """
 
-from rdflib import Graph,URIRef, BNode, Literal, Namespace, RDF, RDFS
+from rdflib import Graph, URIRef, BNode, Literal, Namespace, RDF, RDFS
 from pyld import jsonld
 from datetime import datetime
 from pytz import timezone
 import json
 import uuid
-
 
 # Contexts for SOSA, SSN-EXT, SOSA
 #  SOSA https://github.com/opengeospatial/ELFIE/blob/master/docs/json-ld/sosa.jsonld
@@ -30,100 +29,98 @@ import uuid
 # https://github.com/zimeon/iiif-ld-demo
 
 
-
 context = {
-   "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "owl" : "http://www.w3.org/2002/07/owl#",
-    "ssn-ext-examples" : "http://example.org/ssn-ext-examples#",
-    "xsd" : "http://www.w3.org/2001/XMLSchema#",
-    "dcterms" : "http://purl.org/dc/terms/",
-    "rdfs" : "http://www.w3.org/2000/01/rdf-schema#",
-    "time" : "http://www.w3.org/2006/time#",
-    "ssn-ext" : "http://www.w3.org/ns/ssn/ext/",
-    "sosa" : "http://www.w3.org/ns/sosa/",
-"qudt": "http://qudt.org/1.1/schema/qudt#",
-"prov": "http://www.w3.org/ns/prov#",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "owl": "http://www.w3.org/2002/07/owl#",
+    "ssn-ext-examples": "http://example.org/ssn-ext-examples#",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
+    "dcterms": "http://purl.org/dc/terms/",
+    "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+    "time": "http://www.w3.org/2006/time#",
+    "ssn-ext": "http://www.w3.org/ns/ssn/ext/",
+    "sosa": "http://www.w3.org/ns/sosa/",
+    "qudt": "http://qudt.org/1.1/schema/qudt#",
+    "prov": "http://www.w3.org/ns/prov#",
 
-"hasUltimateFeatureOfInterest" : {
-      "@id" : "http://www.w3.org/ns/ssn/ext/hasUltimateFeatureOfInterest",
-      "@type" : "@id"
+    "hasUltimateFeatureOfInterest": {
+        "@id": "http://www.w3.org/ns/ssn/ext/hasUltimateFeatureOfInterest",
+        "@type": "@id"
     },
-    "usedProcedure" : {
-      "@id" : "http://www.w3.org/ns/sosa/usedProcedure",
-      "@type" : "@id"
+    "usedProcedure": {
+        "@id": "http://www.w3.org/ns/sosa/usedProcedure",
+        "@type": "@id"
     },
-    "phenomenonTime" : {
-      "@id" : "http://www.w3.org/ns/sosa/phenomenonTime",
-      "@type" : "@id"
+    "phenomenonTime": {
+        "@id": "http://www.w3.org/ns/sosa/phenomenonTime",
+        "@type": "@id"
     },
-    "observedProperty" : {
-      "@id" : "http://www.w3.org/ns/sosa/observedProperty",
-      "@type" : "@id"
+    "observedProperty": {
+        "@id": "http://www.w3.org/ns/sosa/observedProperty",
+        "@type": "@id"
     },
-    "madeBySensor" : {
-      "@id" : "http://www.w3.org/ns/sosa/madeBySensor",
-      "@type" : "@id"
+    "madeBySensor": {
+        "@id": "http://www.w3.org/ns/sosa/madeBySensor",
+        "@type": "@id"
     },
-    "hasFeatureOfInterest" : {
-      "@id" : "http://www.w3.org/ns/sosa/hasFeatureOfInterest",
-      "@type" : "@id"
+    "hasFeatureOfInterest": {
+        "@id": "http://www.w3.org/ns/sosa/hasFeatureOfInterest",
+        "@type": "@id"
     },
-    "hasMember" : {
-      "@id" : "http://www.w3.org/ns/ssn/ext/hasMember",
-      "@type" : "@id"
+    "hasMember": {
+        "@id": "http://www.w3.org/ns/ssn/ext/hasMember",
+        "@type": "@id"
     },
-    "inXSDDateTime" : {
-      "@id" : "http://www.w3.org/2006/time#inXSDDateTime",
-      "@type" : "http://www.w3.org/2001/XMLSchema#dateTime"
+    "inXSDDateTime": {
+        "@id": "http://www.w3.org/2006/time#inXSDDateTime",
+        "@type": "http://www.w3.org/2001/XMLSchema#dateTime"
     },
-    "hasBeginning" : {
-      "@id" : "http://www.w3.org/2006/time#hasBeginning",
-      "@type" : "@id"
+    "hasBeginning": {
+        "@id": "http://www.w3.org/2006/time#hasBeginning",
+        "@type": "@id"
     },
-    "isSampleOf" : {
-      "@id" : "http://www.w3.org/ns/sosa/isSampleOf",
-      "@type" : "@id"
+    "isSampleOf": {
+        "@id": "http://www.w3.org/ns/sosa/isSampleOf",
+        "@type": "@id"
     },
-    "hasResult" : {
-      "@id" : "http://www.w3.org/ns/sosa/hasResult",
-      "@type" : "@id"
+    "hasResult": {
+        "@id": "http://www.w3.org/ns/sosa/hasResult",
+        "@type": "@id"
     },
-    "imports" : {
-      "@id" : "http://www.w3.org/2002/07/owl#imports",
-      "@type" : "@id"
+    "imports": {
+        "@id": "http://www.w3.org/2002/07/owl#imports",
+        "@type": "@id"
     },
-    "comment" : {
-      "@id" : "http://www.w3.org/2000/01/rdf-schema#comment"
+    "comment": {
+        "@id": "http://www.w3.org/2000/01/rdf-schema#comment"
     },
-    "creator" : {
-      "@id" : "http://purl.org/dc/terms/creator",
-      "@type" : "@id"
+    "creator": {
+        "@id": "http://purl.org/dc/terms/creator",
+        "@type": "@id"
     },
-    "created" : {
-      "@id" : "http://purl.org/dc/terms/created",
-      "@type" : "http://www.w3.org/2001/XMLSchema#date"
+    "created": {
+        "@id": "http://purl.org/dc/terms/created",
+        "@type": "http://www.w3.org/2001/XMLSchema#date"
     },
-    "resultTime" : {
-      "@id" : "http://www.w3.org/ns/sosa/resultTime",
-      "@type" : "http://www.w3.org/2001/XMLSchema#dateTime"
+    "resultTime": {
+        "@id": "http://www.w3.org/ns/sosa/resultTime",
+        "@type": "http://www.w3.org/2001/XMLSchema#dateTime"
     },
 
-
-"ObservationCollection": "ssn-ext:ObservationCollection",
-"hasMember": "ssn-ext:hasMember",
-"isMemberOf": "ssn-ext:isMemberOf",
-"Observation": "sosa:Observation",
-"Sample": "sosa:Sample",
-"observedProperty": "sosa:observedProperty",
-"hasBeginning": "time:hasBeginning",
-"hasEnd": "time:hasEnd",
-"hasGeometry": "gsp:hasGeometry",
-"isSampleOf": "sosa:isSampleOf",
-"isFeatureOfInterestOf": "sosa:isFeatureOfInterestOf",
-"relatedSample": "sampling:relatedSample",
-"quantityValue": "http://qudt.org/schema/qudt#quantityValue",
-"numericValue": "http://qudt.org/schema/qudt#numericValue",
-"unit": "http://qudt.org/schema/qudt#unit"
+    "ObservationCollection": "ssn-ext:ObservationCollection",
+    "hasMember": "ssn-ext:hasMember",
+    "isMemberOf": "ssn-ext:isMemberOf",
+    "Observation": "sosa:Observation",
+    "Sample": "sosa:Sample",
+    "observedProperty": "sosa:observedProperty",
+    "hasBeginning": "time:hasBeginning",
+    "hasEnd": "time:hasEnd",
+    "hasGeometry": "gsp:hasGeometry",
+    "isSampleOf": "sosa:isSampleOf",
+    "isFeatureOfInterestOf": "sosa:isFeatureOfInterestOf",
+    "relatedSample": "sampling:relatedSample",
+    "quantityValue": "http://qudt.org/schema/qudt#quantityValue",
+    "numericValue": "http://qudt.org/schema/qudt#numericValue",
+    "unit": "http://qudt.org/schema/qudt#unit"
 }
 
 # Add Graph obj
@@ -137,32 +134,52 @@ qudt = Namespace("http://qudt.org/1.1/schema/qudt#")
 owltime = Namespace("ttp://www.w3.org/2006/time#")
 owl = Namespace("http://www.w3.org/2002/07/owl#")
 rdf = Namespace("http://purl.org/dc/terms/")
-rdfs = Namespace ("http://www.w3.org/2000/01/rdf-schema#")
+rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+ssn = Namespace("http://www.w3.org/ns/ssn/")
 
-class Base(object):
-    def __init__(self, type, **kwargs):
-        self.jsonld = {
-            "@type": type,
-        }
-        self.jsonld.update(kwargs)
+def get_graph():
+    return obsgraph
+
+
+class platform(object):
+    """
+    Creates a Platform object that represents a SOSA Platform
+"""
+    # Maybe remove list if makes object too big/not needed, or might want a func that returns this list
+    sensors = []
+
+    def __init__(self, comment, label):
+        self.platform_id = BNode()
+        self.label = Literal(label)
+        self.comment = Literal(comment)
+        obsgraph.add((self.platform_id, RDF.type, sosa.Platform))
+        obsgraph.add((self.platform_id, RDFS.comment, self.comment))
+        obsgraph.add((self.platform_id, RDFS.label, self.label))
+
+    def add_sensor(self, Sensor):
+        a_uri = Sensor.get_uri()
+        self.sensors.append(a_uri)
+        obsgraph.add((self.platform_id, sosa.hosts, a_uri))
+        Sensor.add_platform_id(self.platform_id)
 
 
 class ObservationCollection(object):
     """ Create SSN-EXT Observation Collection """
+
     def __init__(self, comment):
-        self.jsonld={
+        self.jsonld = {
             "@type": "ssn-ext:ObservationCollection",
-            "hasFeatureOfInterest" : "http://example.org/Sample_2",
-            "madeBySensor" : "http://example.org/s4",
-            "observedProperty" : "http://example.org/op2",
-            "phenomenonTime" : "_:b13",
-            "usedProcedure" : "http://example.org/p3",
-            "hasMember" : [ "http://example.org/O5", "http://example.org/O4" ]
+            "hasFeatureOfInterest": "http://example.org/Sample_2",
+            "madeBySensor": "http://example.org/s4",
+            "observedProperty": "http://example.org/op2",
+            "phenomenonTime": "_:b13",
+            "usedProcedure": "http://example.org/p3",
+            "hasMember": ["http://example.org/O5", "http://example.org/O4"]
         }
         self.obscollid = BNode()
         self.comment = Literal(comment)
         obsgraph.add((self.obscollid, RDF.type, ssnext.ObservationCollection))
-        obsgraph.add((self.obscollid, RDFS.comment, self.comment ))
+        obsgraph.add((self.obscollid, RDFS.comment, self.comment))
 
     def addObservation(self, sensorURI, FeatureURI, result):
         obsid = BNode()
@@ -175,30 +192,59 @@ class ObservationCollection(object):
         obsgraph.add((obsid, sosa.resultTime, resultTimeLiteral))
         obsgraph.add((obsid, sosa.hasSimpleResult, resultLiteral))
 
+
 # str(uuid.uuid4())
 
 class Observation(object):
     def __init__(self):
         pass
 
+
 class Sensor(object):
-    def __init__(self, sensorDescription, observablePropertyURI):
+    def __init__(self, sensor_description, observable_property_uri):
         self.sensorid = BNode()
-        self.comment =  Literal(sensorDescription)
+        self.sensor_description = Literal(sensor_description)
         obsgraph.add((self.sensorid, RDF.type, sosa.Sensor))
-        obsgraph.add((self.sensorid, sosa.Observes, observablePropertyURI))
-        obsgraph.add((self.sensorid, RDFS.comment, self.comment))
+        obsgraph.add((self.sensorid, sosa.Observes, observable_property_uri))
+        obsgraph.add((self.sensorid, RDFS.comment, self.sensor_description))
+
+    def add_platform_id(self, platform_id):
+        obsgraph.add((self.sensorid, sosa.isHostedBy, platform_id))
+
+    def get_uri(self):
+        return self.sensorid
+
+    def add_obs_property(self, observable_property):
+        obsgraph.add(self.sensorid, sosa.observes, observable_property)
+
 
 # Class for managing observableproperties
 # Preferably linked to envo, sweet and qudt
-class ObservableProperty:
-    def __init__(self, observablePropertyURI):
-        self.observableProperty = observablePropertyURI
-        obsgraph.add((self.observablePropertyURI, rdf.type, sosa.ObservableProperty))
+class ObservableProperty(object):
+    """
+    Creates a Observable Property object that represents a SOSA Observable Property
+
+    """
+
+    def __init__(self, property_uri):
+        if property_uri:
+            self.observable_property_uri = property_uri
+        else:
+            self.observable_property_uri = BNode()
+        obsgraph.add((self.observable_property_uri, rdf.type, sosa.Observable_property))
+
+    def get_uri(self):
+        return self.observable_property_uri
+
+
 class FeatureOfInterest(object):
+"""   Creates a Feature of Interest object that represents a SOSA Feature of Interest """
+
     def __init__(self):
         self.uri = "_B0"
         pass
+
+
 class UltimateFeatureOfInterest(FeatureOfInterest):
     def __init__(self):
         super(UltimateFeatureOfInterest, self).__init__()
--- tests/test_pyobs.py
+++ tests/test_pyobs.py
@@ -1,4 +1,11 @@
 from pyobs import pyobs
+import rdflib
+
 
 def test_Observation():
-    obs = pyobs.ObservationCollection()
+    obs = pyobs.ObservationCollection(comment="mycol")
+    this_graph = pyobs.get_graph()
+    print(this_graph.serialize(format='turtle'))
+
+if __name__ == '__main__':
+    test_Observation()
