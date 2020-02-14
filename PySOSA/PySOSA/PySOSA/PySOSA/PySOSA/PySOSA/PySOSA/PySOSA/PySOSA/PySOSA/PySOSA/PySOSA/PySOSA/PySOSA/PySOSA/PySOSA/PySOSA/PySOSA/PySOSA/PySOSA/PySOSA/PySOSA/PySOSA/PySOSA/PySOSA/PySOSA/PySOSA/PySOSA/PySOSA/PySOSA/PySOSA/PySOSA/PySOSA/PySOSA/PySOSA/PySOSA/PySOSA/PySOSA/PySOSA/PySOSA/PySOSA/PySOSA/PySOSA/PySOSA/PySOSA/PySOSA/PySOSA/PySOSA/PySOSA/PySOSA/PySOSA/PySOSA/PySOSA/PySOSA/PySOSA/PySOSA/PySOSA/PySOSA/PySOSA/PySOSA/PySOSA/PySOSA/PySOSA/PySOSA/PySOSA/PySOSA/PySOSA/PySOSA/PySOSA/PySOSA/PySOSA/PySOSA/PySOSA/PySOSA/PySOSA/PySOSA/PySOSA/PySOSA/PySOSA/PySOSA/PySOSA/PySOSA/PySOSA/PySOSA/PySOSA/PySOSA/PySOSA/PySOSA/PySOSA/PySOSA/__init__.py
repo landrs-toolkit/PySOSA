b46e35f5c1f15cbb773c88bb85d85ddebd47e287
BadisaMosesane <badisamosne@gmail.com>
2020-02-14 21:37:06 +0200

    updated LICENSE

--- PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/PySOSA/__init__.py
+++
@@ -1,5 +0,0 @@
-from rdflib import Graph, URIRef, BNode, Literal, Namespace, RDF, RDFS
-from PySOSA.landrs_module import Platform, Sensor, Observation
-from PySOSA.landrs_module import FeatureOfInterest, ObservableProperty
-from PySOSA.landrs_module import Actuator, Procedure, Actuation, Stimulus, ActuableProperty
-from pyobs_pyobs import obsgraph, sosa, Sensor, ssnext
--- PySOSA/docs/pyobs_pyobs.rst
+++
@@ -1,7 +0,0 @@
-pyobs\_pyobs module
-===================
-
-.. automodule:: pyobs_pyobs
-    :members:
-    :undoc-members:
-    :show-inheritance:
